afe9f109cd6037a6bc2165daf98dac862429e3a1
cysi4k <cysi4k@gmail.com>
2019-06-01 04:06:46 +0200

    Add Search for Plant Page

--- app/src/main/java/com/example/gardenflow/FindPlant.java
+++ app/src/main/java/com/example/gardenflow/FindPlant.java
@@ -1,13 +1,31 @@
 package com.example.gardenflow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FindPlant extends AppCompatActivity {
+    private Button searchForPlantButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_plant);
+
+        searchForPlantButton = (Button) findViewById(R.id.searchForPlantButton);
+        searchForPlantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSearchForPlantActivity();
+            }
+        });
+    }
+
+    public void openSearchForPlantActivity() {
+        Intent intent = new Intent(this, SearchForPlant.class);
+        startActivity(intent);
+
     }
 }
